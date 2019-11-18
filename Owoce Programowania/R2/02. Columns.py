ad348d6a8d397cee8939d222ac3d0c5b06692fce
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-11-18 01:26:29 +0100

    Create 02. Columns.py

---
+++ Owoce Programowania/R2/02. Columns.py
@@ -0,0 +1,29 @@
+# Ten program wyświetla poniższe liczby
+# zmiennoprzecinkowe w kolumnie wyrównane
+# według położenia przecinka dziesiętnego.
+num1 = 127.899
+num2 = 3465.148
+num3 = 3.776
+num4 = 264.821
+num5 = 88.081
+num6 = 799.999
+
+# Wyświetlenie w polu o szerokości siedmiu znaków
+# wszystkich liczb wraz z dwoma cyframi po przecinku dziesiętnym.
+print(format(num1, '7.2f'))
+print(format(num2, '7.2f'))
+print(format(num3, '7.2f'))
+print(format(num4, '7.2f'))
+print(format(num5, '7.2f'))
+print(format(num6, '7.2f'))
+
+
+
+Run:
+
+ 127.90
+3465.15
+   3.78
+ 264.82
+  88.08
+ 800.00
