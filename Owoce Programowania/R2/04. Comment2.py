d39b377a38f5b800e16e24610381f9a8ac39a544
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-11-23 23:27:43 +0100

    Create 04. Comment2.py

---
+++ Owoce Programowania/R2/04. Comment2.py
@@ -0,0 +1,3 @@
+print('Jan Kowalski')       # Wyświetlenie imienia i nazwiska.
+print('ul. Ogrodowa 123')   # Wyświetlenie adresu.
+print('44-100 Gliwice')     # Wyświetlenie kodu pocztowego i miejscowości.
