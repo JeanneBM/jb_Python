a47cf5004d9b508a93a0e7fbab065b23569aab1c
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-11-24 14:27:57 +0100

    Create 08. Formatting.py

---
+++ Owoce Programowania/R2/08. Formatting.py
@@ -0,0 +1,5 @@
+# Ten program pokazuje sposób wyświetlenia
+# liczby zmiennoprzecinkowej wraz z formatowaniem.
+amount_due = 5000.0
+monthly_payment = amount_due / 12
+print('Miesięczna rata wynosi', format(monthly_payment, '.2f'))
