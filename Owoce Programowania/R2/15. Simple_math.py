280c4dde646e307df97d39c0a7009d9b9f6eecdc
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-11-24 14:38:52 +0100

    Create 15. Simple_math.py

---
+++ Owoce Programowania/R2/15. Simple_math.py
@@ -0,0 +1,12 @@
+# Przypisanie wartości zmiennej salary.
+salary = 2500.0
+
+# Przypisanie wartości zmiennej bonus.
+bonus = 1200.0
+
+# Obliczenie wynagrodzenia przez dodanie wartości zmiennych
+# salary i bonus. Wynik zostaje przypisany zmiennej pay.
+pay = salary + bonus
+
+# Wyświetlenie wynagrodzenia.
+print('Twoje wynagrodzenie wynosi', pay)
