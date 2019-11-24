bef800c0dc61d31315a82519288c623bd3e73900
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-11-24 14:31:56 +0100

    Create 10. Input.py

---
+++ Owoce Programowania/R2/10. Input.py
@@ -0,0 +1,10 @@
+# Pobranie imienia, wieku i dochodu użytkownika.
+name = input('Jak masz na imię? ')
+age = int(input('Ile masz lat? '))
+income = float(input('Ile zarabiasz? '))
+
+# Wyświetlenie danych.
+print('Oto wprowadzone przez Ciebie dane.')
+print('Imię:', name)
+print('Wiek:', age)
+print('Dochód:', income)
