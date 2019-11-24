3cfa3ba2785c2cfda8e59a9084ea4d63062750d7
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-11-24 14:32:49 +0100

    Create 11. No_formatting.py

---
+++ Owoce Programowania/R2/11. No_formatting.py
@@ -0,0 +1,5 @@
+# Ten program pokazuje sposób wyświetlenia
+# liczby zmiennoprzecinkowej bez formatowania.
+amount_due = 5000.0
+monthly_payment = amount_due / 12.0
+print('Miesięczna rata wynosi', monthly_payment)
