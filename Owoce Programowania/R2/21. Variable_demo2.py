2b84d179744a1fe3dc5e1e9fbef8bfab1feb2744
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-11-26 00:59:12 +0100

    Create 21. Variable_demo2.py

---
+++ Owoce Programowania/R2/21. Variable_demo2.py
@@ -0,0 +1,9 @@
+# Utworzenie dwóch zmiennych: top_speed i distance.
+top_speed = 160
+distance = 300
+
+# Wyświetlenie wartości przechowywanych przez te zmienne.
+print('Szybkość maksymalna wynosi')
+print(top_speed)
+print('Pokonana odległość to')
+print(distance)
