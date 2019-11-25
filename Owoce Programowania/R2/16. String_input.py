42896a9d6142cefd24e2596f58f3c9d0c3dc6089
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-11-26 00:52:07 +0100

    Create 16. String_input.py

---
+++ Owoce Programowania/R2/16. String_input.py
@@ -0,0 +1,8 @@
+# Pobranie imienia użytkownika.
+first_name = input('Podaj imię: ')
+
+# Pobranie nazwiska użytkownika.
+last_name = input('Podaj nazwisko: ')
+
+# Wyświetlenie komunikatu powitania.
+print('Witaj,', first_name, last_name)
