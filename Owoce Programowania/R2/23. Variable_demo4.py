3b9e7e5ad134c888d6c368bcc2451edb08c32b44
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-11-26 01:01:01 +0100

    Create 23. Variable_demo4.py

---
+++ Owoce Programowania/R2/23. Variable_demo4.py
@@ -0,0 +1,9 @@
+# Ten program pokazuje ponowne przypisanie zmiennej.
+# Wartość jest przypisywana zmiennej dollars.
+dollars = 2.75
+print('Mam na koncie', dollars, 'dolarów.')
+
+# Ponowne przypisanie zmiennej, która
+# teraz odwołuje się do innej wartości.
+dollars = 99.95
+print('A teraz mam na koncie', dollars, 'dolarów.')
