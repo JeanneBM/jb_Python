75ecbe21f6630df552123ad54e4505ade613cc48
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-11-29 22:50:00 +0100

    Create 07. Password.py

---
+++ Owoce Programowania/R03/07. Password.py
@@ -0,0 +1,10 @@
+# Ten program porównuje dwa ciągi tekstowe.
+# Pobranie hasła od użytkownika.
+password = input('Podaj hasło: ')
+
+# Ustalenie, czy użytkownik
+# podał prawidłowe hasło.
+if password == 'prospero':
+    print('Hasło jest poprawne.')
+else:
+    print('Podane hasło jest niepoprawne.')
