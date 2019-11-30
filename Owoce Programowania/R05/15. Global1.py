820006a3375308a97b590bb132a6ce4498b0cb9a
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-11-30 23:44:42 +0100

    Create 15. Global1.py

---
+++ Owoce Programowania/R05/15. Global1.py
@@ -0,0 +1,12 @@
+# Utworzenie zmiennej globalnej.
+my_value = 10
+
+# Funkcja show_value() wyświetla
+# wartość zmiennej globalnej.
+def show_value():
+    print(my_value)
+
+# Wywołanie funkcji show_value().
+show_value()
+
+
