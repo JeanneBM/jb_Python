47308eb4a14499350cea0f60219d800bf9142562
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-11-30 23:23:34 +0100

    Create 10. Simple_loop2.py

---
+++ Owoce Programowania/R04/10. Simple_loop2.py
@@ -0,0 +1,8 @@
+# Ten program również przedstawia prostą pętlę for,
+# która wykorzystuje listę liczb.
+
+print('Teraz będą wyświetlone liczby nieparzyste od 1 do 9.')
+for num in [1, 3, 5, 7, 9]:
+    print(num)
+
+
