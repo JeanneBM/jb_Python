c2f6c2bed72c44c6d9b918f74e8c3c290518cf2b
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-11-30 23:48:33 +0100

    Create 21. Multiple_args.py

---
+++ Owoce Programowania/R05/21. Multiple_args.py
@@ -0,0 +1,17 @@
+# Ten program pokazuje przykład
+# funkcji akceptującej dwa argumenty.
+
+def main():
+    print('Suma liczb 12 i 45 wynosi')
+    show_sum(12, 45)
+
+# Funkcja show_sum() akceptuje dwa
+# argumenty i wyświetla ich sumę.
+def show_sum(num1, num2):
+    result = num1 + num2
+    print(result)
+
+# Wywołanie funkcji main().
+main()
+
+
