c7e4436f626ee2df71dae4e1d7ac5ca0ff22392c
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-11-30 23:43:37 +0100

    Create 13. Function_demo.py

---
+++ Owoce Programowania/R05/13. Function_demo.py
@@ -0,0 +1,10 @@
+# Ten program pokazuje przykład funkcji.
+# Najpierw należy zdefiniować funkcję o nazwie message().
+def message():
+    print('Jestem Artur,')
+    print('król Brytyjczyków.')
+
+# Wywołanie funkcji message().
+message()
+
+
