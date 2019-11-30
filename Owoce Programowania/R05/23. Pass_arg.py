7d046068c66803cbf9ef1c610baf0b68bcea65f3
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-11-30 23:50:17 +0100

    Create 23. Pass_arg.py

---
+++ Owoce Programowania/R05/23. Pass_arg.py
@@ -0,0 +1,17 @@
+# Ten program pokazuje przykład
+# przekazania argumentu funkcji.
+
+def main():
+    value = 5
+    show_double(value)
+
+# Funkcja show_double() akceptuje argument
+# i wyświetla jego podwojoną wartość.
+def show_double(number):
+    result = number * 2
+    print(result)
+
+# Wywołanie funkcji main().
+main()
+
+
