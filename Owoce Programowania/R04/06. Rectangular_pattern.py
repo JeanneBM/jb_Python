a98cfa878f39b3d65f9447e1c4768ab0c5e0d994
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-11-30 23:20:48 +0100

    Create 06. Rectangular_pattern.py

---
+++ Owoce Programowania/R04/06. Rectangular_pattern.py
@@ -0,0 +1,20 @@
+# Ten program wyświetla prostokątny
+# kształt zbudowany z gwiazdek.
+rows = int(input('Podaj liczbę wierszy: '))
+cols = int(input('Podaj licz# Ten program wyświetla prostokątny
+# kształt zbudowany z gwiazdek.
+rows = int(input('Podaj liczbę wierszy: '))
+cols = int(input('Podaj liczbę kolumn: '))
+
+for r in range(rows):
+    for c in range(cols):
+        print('*', end='')
+    print()
+bę kolumn: '))
+
+for r in range(rows):
+    for c in range(cols):
+        print('*', end='')
+    print()
+    
+    
