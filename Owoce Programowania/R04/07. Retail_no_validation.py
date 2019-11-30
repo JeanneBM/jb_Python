a1c57b50314586499cf6cc65729ebd3e7e36464c
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-11-30 23:21:31 +0100

    Create 07. Retail_no_validation.py

---
+++ Owoce Programowania/R04/07. Retail_no_validation.py
@@ -0,0 +1,22 @@
+# Ten program oblicza cenę detaliczną produktu.
+
+MARK_UP = 2.5  # Procentowa wysokość marży.
+another = 't'  # Zmienna kontrolująca działanie pętli.
+
+# Przetworzenie jednego lub większej liczby produktów.
+while another == 't' or another == 'T':
+    # Pobranie kosztu całkowitego produktu.
+    wholesale = float(input("Podaj cenę hurtową " +
+                            "produktu: "))
+
+    # Obliczenie ceny detalicznej.
+    retail = wholesale * MARK_UP
+
+    # Wyświetlenie ceny detalicznej.
+    print('Cena detaliczna: ', format(retail, '.2f'), ' zł.', sep='')
+
+    # Czy powtórzyć operację?
+    another = input('Czy chcesz obliczyć cenę innego produktu? ' +
+                    '(Jeśli tak, wpisz t): ')
+
+
