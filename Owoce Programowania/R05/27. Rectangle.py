bbdb52159d395e455e1d484af61287e85e600b65
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-11-30 23:52:42 +0100

    Create 27. Rectangle.py

---
+++ Owoce Programowania/R05/27. Rectangle.py
@@ -0,0 +1,15 @@
+# Moduł rectangle zawiera funkcje przeprowadzające
+# obliczenia związane z prostokątem.
+
+# Funkcja area() akceptuje argumenty w postaci długości
+# i szerokości prostokąta i zwraca jego pole powierzchni.
+def area(width, length):
+    return width * length
+
+# Funkcja perimeter() akceptuje argumenty w postaci
+# długości i szerokości prostokąta i zwraca jego
+# obwód.
+def perimeter(width, length):
+    return 2 * (width + length)
+
+
