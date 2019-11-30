c7b16a2f416bfdd52f10ceccde41b7c70b3fc272
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-11-30 23:37:57 +0100

    Create 05. Circle.py

---
+++ Owoce Programowania/R05/05. Circle.py
@@ -0,0 +1,15 @@
+# Moduł circle zawiera funkcje przeprowadzające
+# obliczenia związane z okręgiem.
+import math
+
+# Funkcja area() akceptuje argument w postaci
+# promienia okręgu i zwraca pole powierzchni.
+def area(radius):
+    return math.pi * radius**2
+
+# Funkcja circumference() akceptuje argument w postaci
+# promienia okręgu i zwraca obwód okręgu.
+def circumference(radius):
+    return 2 * math.pi * radius
+
+
