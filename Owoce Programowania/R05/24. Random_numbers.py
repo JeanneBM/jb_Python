03ec27052d62ff4365d82f7179cf2bc411927173
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-11-30 23:50:47 +0100

    Create 24. Random_numbers.py

---
+++ Owoce Programowania/R05/24. Random_numbers.py
@@ -0,0 +1,14 @@
+# Ten program wyświetla liczbę losową
+# z przedziału od 1 do 10.
+import random
+
+def main():
+    # Wygenerowanie liczby losowej.
+    number = random.randint(1, 10)
+    # Wyświetlenie wygenerowanej liczby.
+    print('Liczba to', number)
+
+# Wywołanie funkcji main().
+main()
+
+
