5a6937f476e13895927bd0e7019f96c5690995d5
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-12-06 23:51:15 +0100

    Create 20. Sales_report2.py

---
+++ Owoce Programowania/R06/20. Sales_report2.py
@@ -0,0 +1,28 @@
+# Ten program wyświetla sumę wszystkich
+# wartości zapisanych w pliku sales_data.txt.
+
+def main():
+    # Inicjalizacja zmiennej akumulatora.
+    total = 0.0
+
+    try:
+        # Otworzenie pliku sales_data.txt.
+        infile = open('sales_data.txt', 'r')
+
+        # Odczytanie wartości z pliku,
+        # a następnie ich zsumowanie.
+        for line in infile:
+            amount = float(line)
+            total += amount
+
+        # Zamknięcie pliku.
+        infile.close()
+
+        # Wyświetlenie obliczonej sumy.
+        print(format(total, '.2f'))
+    except:
+        print('Wystąpił błąd.')
+
+# Wywołanie funkcji main().
+main()
+
