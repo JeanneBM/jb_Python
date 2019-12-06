edd484725dc115ef53c148a1100760e92018763c
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-12-06 23:56:45 +0100

    Create 02. Bar_chart1.py

---
+++ Owoce Programowania/R07/02. Bar_chart1.py
@@ -0,0 +1,18 @@
+# Ten program wyświetla prosty wykres słupkowy.
+import matplotlib.pyplot as plt
+
+def main():
+    # Utworzenie listy wraz ze współrzędnymi X lewej krawędzi wszystkich słupków.
+    left_edges = [0, 10, 20, 30, 40]
+
+    # Utworzenie listy wraz z wysokością wszystkich słupków.
+    heights = [100, 200, 300, 400, 500]
+
+    # Utworzenie wykresu słupkowego.
+    plt.bar(left_edges, heights)
+
+    # Wyświetlenie wykresu słupkowego.
+    plt.show()
+
+# Wywołanie funkcji main().
+main()
