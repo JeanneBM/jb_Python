551a260cd80677efaf4cbe8ab1d8dc0b095abe4c
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-12-06 23:42:03 +0100

    Create 33. Two_functions.py

---
+++ Owoce Programowania/R05/33. Two_functions.py
@@ -0,0 +1,14 @@
+# Ten program zawiera dwie funkcje.
+# Najpierw trzeba zdefiniować funkcję main().
+def main():
+    print('Mam dla Ciebie wiadomość.')
+    message()
+    print('Żegnaj!')
+
+# Kolejnym krokiem jest zdefiniowanie funkcji message().
+def message():
+    print('Jestem Artur,')
+    print('król Brytyjczyków.')
+
+# Wywołanie funkcji main().
+main()
