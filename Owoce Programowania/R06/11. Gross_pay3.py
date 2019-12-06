d84f1c58cd6bc7eafe524222c4d452df91acabb3
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-12-06 23:47:25 +0100

    Create 11. Gross_pay3.py

---
+++ Owoce Programowania/R06/11. Gross_pay3.py
@@ -0,0 +1,20 @@
+# Ten program oblicza wynagrodzenie pracownika.
+
+def main():
+    try:
+        # Pobranie liczby przepracowanych godzin.
+        hours = int(input('Podaj liczbę przepracowanych godzin: '))
+
+        # Pobranie stawki godzinowej.
+        pay_rate = float(input('Podaj stawkę godzinową: '))
+
+        # Obliczenie należnego wynagrodzenia.
+        gross_pay = hours * pay_rate
+
+        # Wyświetlenie obliczonego wynagrodzenia.
+        print('Wynagrodzenie wynosi ', format(gross_pay, '.2f'), ' zł', sep='')
+    except ValueError as err:
+        print(err)
+
+# Wywołanie funkcji main().
+main()
