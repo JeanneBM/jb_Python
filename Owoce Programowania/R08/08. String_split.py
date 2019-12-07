10f31aadddedbd0ad08196312d3c3f44c366fe62
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-12-07 19:09:16 +0100

    Create 08. String_split.py

---
+++ Owoce Programowania/R08/08. String_split.py
@@ -0,0 +1,14 @@
+# Ten program pokazuje przykład użycia metody split().
+
+def main():
+    # Utworzenie ciągu tekstowego wraz z wieloma słowami.
+    my_string = 'jeden dwa trzy cztery'
+
+    # Podział ciągu tekstowego.
+    word_list = my_string.split()
+
+    # Wyświetlenie listy słów.
+    print(word_list)
+
+# Wywołanie funkcji main().
+main()
