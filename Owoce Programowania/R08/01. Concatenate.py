f851839ef7e2805b72387da9108ef2b918bfe5e6
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-12-07 19:06:17 +0100

    Create 01. Concatenate.py

---
+++ Owoce Programowania/R08/01. Concatenate.py
@@ -0,0 +1,10 @@
+# Ten program pokazuje konkatenację ciągów tekstowych.
+
+def main():
+    name = 'Karina'
+    print('Imię to', name)
+    name = name + ' Malinowska'
+    print('Teraz imię i nazwisko to', name)
+
+# Wywołanie funkcji main().
+main()
