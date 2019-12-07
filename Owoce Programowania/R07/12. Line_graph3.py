988c5e6fd458d57ef54dbd0fb613fb6036f36e99
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-12-07 18:48:31 +0100

    Create 12. Line_graph3.py

---
+++ Owoce Programowania/R07/12. Line_graph3.py
@@ -0,0 +1,30 @@
+# Ten program wyświetla prosty wykres liniowy.
+import matplotlib.pyplot as plt
+
+def main():
+    # Utworzenie list wraz ze współrzędnymi X i Y każdego punktu danych.
+    x_coords = [0, 1, 2, 3, 4]
+    y_coords = [0, 3, 1, 5, 2]
+
+    # Utworzenie wykresu.
+    plt.plot(x_coords, y_coords)
+
+    # Dodanie tytułu.
+    plt.title('Przykładowe dane')
+
+    # Dodanie etykiet do osi.
+    plt.xlabel('To jest oś X')
+    plt.ylabel('To jest oś Y')
+
+    # Określenie granic osi.
+    plt.xlim(xmin=-1, xmax=10)
+    plt.ylim(ymin=-1, ymax=6)
+
+    # Dodanie siatki.
+    plt.grid(True)
+
+    # Wyświetlenie wykresu.
+    plt.show()
+
+# Wywołanie funkcji main().
+main()
