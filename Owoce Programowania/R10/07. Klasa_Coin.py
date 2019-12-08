fdf3b1370b421748fb5340c30f79cd07323b8032
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-12-08 23:26:35 +0100

    Create 07. Klasa_Coin.py

---
+++ Owoce Programowania/R10/07. Klasa_Coin.py
@@ -0,0 +1,29 @@
+import random
+
+# Klasa Coin symuluje rzut monetą, w przypadku
+# której może wypaść orzeł lub reszka.
+
+class Coin:
+
+    # Metoda __init__() inicjalizuje atrybut
+    # danych sideup o wartości 'orzeł'.
+
+    def __init__(self):
+        self.sideup = 'orzeł'
+
+    # Metoda toss() generuje liczbę losową w przedziale
+    # od 0 do 1. Jeżeli liczba to 0, wówczas atrybut danych
+    # sideup ma wartość 'orzeł'. W przeciwnym razie jego
+    # wartością jest 'reszka'.
+
+    def toss(self):
+        if random.randint(0, 1) == 0:
+            self.sideup = 'orzeł'
+        else:
+            self.sideup = 'reszka'
+
+    # Metoda get_sideup() zwraca wartość
+    # przechowywaną przez atrybut danych sideup.
+
+    def get_sideup(self):
+        return self.sideup
