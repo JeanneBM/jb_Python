b7494a92178f211333f3fa376dde7e8bf4b6d257
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-12-09 16:53:56 +0100

    Create 01. Endless_recursion.py

---
+++ Owoce Programowania/R12/01. Endless_recursion.py
@@ -0,0 +1,11 @@
+# Ten program zawiera funkcję rekurencyjną.
+
+def main():
+    message()
+
+def message():
+    print('To jest funkcja rekurencyjna.')
+    message()
+
+# Wywołanie funkcji main().
+main()
