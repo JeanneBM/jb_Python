aa69a2d81037277b62863e8c2e4afb5be15db566
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-12-09 18:31:39 +0100

    Create 11. Empty_window.py

---
+++ Owoce Programowania/R13/11. Empty_window.py
@@ -0,0 +1,13 @@
+# Ten program wyświetla puste okno.
+
+import tkinter
+
+def main():
+    # Utworzenie widżetu okna głównego.
+    main_window = tkinter.Tk()
+
+    # Wejście do pętli głównej tkinter.
+    tkinter.mainloop()
+
+# Wywołanie funkcji main().
+main()
