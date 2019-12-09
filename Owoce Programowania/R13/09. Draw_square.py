a379ca06a3ef5df5db54fa5dee797e66cb06adb7
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-12-09 18:30:23 +0100

    Create 09. Draw_square.py

---
+++ Owoce Programowania/R13/09. Draw_square.py
@@ -0,0 +1,22 @@
+# Ten program rysuje prostokąt w widżecie Canvas.
+import tkinter
+
+class MyGUI:
+    def __init__(self):
+        # Utworzenie okna głównego.
+        self.main_window = tkinter.Tk()
+
+        # Utworzenie widżetu Canvas.
+        self.canvas = tkinter.Canvas(self.main_window, width=200, height=200)
+
+        # Narysowanie prostokąta.
+        self.canvas.create_rectangle(20, 20, 180, 180)
+
+        # Wywołanie metody pack() widżetu Canvas.
+        self.canvas.pack()
+
+        # Wywołanie metody mainloop() modułu tkinter.
+        tkinter.mainloop()
+
+# Utworzenie egzemplarza klasy MyGUI.
+my_gui = MyGUI()
