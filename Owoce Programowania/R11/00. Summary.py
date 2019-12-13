320f894ece8f9284dbd149c632328d29b3bf1502
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-12-13 19:00:34 +0100

    Create 00. Summary.py

---
+++ Owoce Programowania/R11/00. Summary.py
@@ -0,0 +1,17 @@
+01. SavingsAccount.py 	
+
+02. Account_demo.py 
+
+03. Animals.py 	
+
+04. Wrong_type.py 
+
+05. Polymorphism_demo1.py 	
+
+06. Polymorphism_demo2.py 
+
+07. Vehicles.py 	
+
+08. Car_demo.py 
+
+09. Car_truck_suv_demo.py
