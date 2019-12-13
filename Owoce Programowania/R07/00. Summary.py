e713c5beb0eb2b250af07f1a7b7b9da2108668ab
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-12-13 18:55:41 +0100

    Update 00. Summary.py

--- Owoce Programowania/R07/00. Summary.py
+++ Owoce Programowania/R07/00. Summary.py
@@ -1,28 +1,55 @@
- 	01. Average_list.py 	
-	02. Bar_chart1.py 	
-	03. Bar_chart2.py 	
-	04. Bar_chart3.py 	
-	05. Barista_pay.py 	
-	06. Drop_lowest_score.py 
-	07. In_list.py 	
-	08. Index_list.py 	
-	09. Insert_list.py 	
-	10. Line_graph1.py 	
-	11. Line_graph2.py 	
-	12. Line_graph3.py 
-	13. Line_graph4.py 	
-	14. Line_graph5.py 	
-	15. List_append.py 	
-	16. Pie_chart1.py 	
-	17. Pie_chart2.py 
-	18. Random_numbers.py 	
-	19. Read_list.py 
-	20. Read_number_list.py 
-	21. Remove_item.py 
-	22. Return_list.py 	
-	23. Sales_list.py 
-	24. Total_function.py 
-	25. Total_list.py 	
-	26. Write_list.py 	
-	27. Write_number_list.py 	
-	28. Write_lines.py
+01. Average_list.py 	
+
+02. Bar_chart1.py 	
+
+03. Bar_chart2.py 	
+
+04. Bar_chart3.py 	
+
+05. Barista_pay.py 	
+
+06. Drop_lowest_score.py 
+
+07. In_list.py 	
+
+08. Index_list.py 	
+
+09. Insert_list.py 	
+
+10. Line_graph1.py 	
+
+11. Line_graph2.py 	
+
+12. Line_graph3.py 
+
+13. Line_graph4.py 	
+
+14. Line_graph5.py 	
+
+15. List_append.py 	
+
+16. Pie_chart1.py 	
+
+17. Pie_chart2.py 
+
+18. Random_numbers.py 	
+
+19. Read_list.py 
+
+20. Read_number_list.py 
+
+21. Remove_item.py 
+
+22. Return_list.py 	
+
+23. Sales_list.py 
+
+24. Total_function.py 
+
+25. Total_list.py 	
+
+26. Write_list.py 	
+
+27. Write_number_list.py 	
+
+28. Write_lines.py
