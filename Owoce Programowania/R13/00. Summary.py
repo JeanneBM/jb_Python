8d86bf27fac78b65cd290081fc43ad2da016bb84
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-12-13 19:04:59 +0100

    Create 00. Summary.py

---
+++ Owoce Programowania/R13/00. Summary.py
@@ -0,0 +1,44 @@
+01. Button_demo.py 	
+
+02. Checkbutton_demo.py 	
+
+03. Draw_arc.py 	
+
+04. Draw_line.py 	
+
+05. Draw_multilines.py 	
+
+06. Draw_ovals.py 	
+
+07. Draw_piechart.py 	
+
+08. Draw_polygon.py 
+
+09. Draw_square.py 
+
+10. Draw_text.py 
+
+11. Empty_window.py 	
+
+12. Empty_window2.py 
+
+13. Font_demo.py 	
+
+14. Frame_demo.py 	
+
+15. Hello_world.py 
+
+16. Hello_world2.py 	
+
+17. Hello_world3.py 
+
+18. Kilo_converter.py 	
+
+19. Kilo_converter2.py 	
+
+20. Quit_button.py 
+
+21. Radiobutton_demo.py 	
+
+22. Test_averages.py
+
